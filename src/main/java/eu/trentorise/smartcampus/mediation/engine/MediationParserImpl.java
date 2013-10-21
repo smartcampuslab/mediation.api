93b27fe424e1081911cd5ab3ef19d083a9e0445e
federicolucca <federico.lucca@gmail.com>
2013-10-21 09:20:22 +0200

    -change model

--- src/main/java/eu/trentorise/smartcampus/mediation/engine/MediationParserImpl.java
+++ src/main/java/eu/trentorise/smartcampus/mediation/engine/MediationParserImpl.java
@@ -1,7 +1,6 @@
 package eu.trentorise.smartcampus.mediation.engine;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -9,7 +8,8 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
-import eu.trentorise.smartcampus.mediation.model.CommentBaseEntity;
+
+import eu.trentorise.smartcampus.mediation.model.KeyWordPersistent;
 import eu.trentorise.smartcampus.mediation.model.MessageToMediationService;
 import eu.trentorise.smartcampus.mediation.util.MediationConstant;
 import eu.trentorise.smartcampus.mediation.util.TextReader;
@@ -17,88 +17,110 @@ import eu.trentorise.smartcampus.network.RemoteConnector;
 import eu.trentorise.smartcampus.network.RemoteException;
 
 public class MediationParserImpl {
-	
+
 	private DataSource dataSource;
 	private String urlServermediation;
 	private String webappname;
-	private String client_token="bearer blabla";
-	
+	private String client_token = "bearer blabla";
+
 	private static final Logger logger = Logger
 			.getLogger(MediationParserImpl.class);
-	
-	public MediationParserImpl(){}
 
-	public MediationParserImpl(DataSource dataSource,String urlServermediation,String webappname){
-		this.dataSource=dataSource;	
-		this.setUrlServermediation(urlServermediation);
-		this.webappname=webappname;
+	public MediationParserImpl() {
 	}
-	
-	
-//	public boolean[] validateComments(List<CommentBaseEntity> entities, String token){
-//		boolean[] returnApprovedState=new boolean[entities.size()];
-//		
-//		int i=0;
-//		for(CommentBaseEntity index : entities){
-//			returnApprovedState[i]=validateComment(index,token);
-//			i++;
-//		}
-//		
-//		return returnApprovedState;
-//		
-//		
-//	}
-	
-	public boolean validateComment(String testoentity,int identity,Long userid, String token){
-		
-		MessageToMediationService messageToMediationService=new MessageToMediationService(webappname,identity,testoentity,String.valueOf(userid));
-		Collection<String> x=getNotApprovedWordDictionary();
-		Iterator<String> index=x.iterator();
-		
-		boolean isApproved=true;
-		
+
+	public MediationParserImpl(DataSource dataSource,
+			String urlServermediation, String webappname) {
+		this.dataSource = dataSource;
+		this.setUrlServermediation(urlServermediation);
+		this.webappname = webappname;
+	}
+
+	public boolean fastValidateComment(String testoentity, int identity,
+			Long userid, String token) {
+
+		MessageToMediationService messageToMediationService = new MessageToMediationService(
+				webappname, identity, testoentity, String.valueOf(userid));
+		Collection<String> x = getNotApprovedWordDictionary();
+		Iterator<String> index = x.iterator();
+
+		boolean isApproved = true;
+
 		long after = 0;
 		long before = 0;
 		long diff = 0;
-		
-		while(index.hasNext() && isApproved){	
-			
+
+		while (index.hasNext() && isApproved) {
+
 			before = System.currentTimeMillis();
-			String test=index.next();
-			isApproved=(testoentity.indexOf(test)==-1);
-			if(!isApproved)	{	
+			String test = index.next();
+			isApproved = (testoentity.indexOf(test) == -1);
+			if (!isApproved) {
 				messageToMediationService.setParseApproved(isApproved);
-				messageToMediationService.setNote("[Blocked by = "+test+"]");
-				addCommentToMediationService(messageToMediationService,token);
+				messageToMediationService
+						.setNote("[Blocked by = " + test + "]");
+				addCommentToMediationService(messageToMediationService, token);
 				after = System.currentTimeMillis();
-				diff = after-before;
-				logger.info("Time parsing = "+diff+" millisec");
-				return isApproved;		
+				diff = after - before;
+				logger.info("Time parsing = " + diff + " millisec");
+				return isApproved;
 			}
 		}
-			
+
 		after = System.currentTimeMillis();
-		diff = after-before;
-		logger.info("Time parsing = "+diff+" millisec");
-		messageToMediationService.setParseApproved(isApproved);		
-		addCommentToMediationService(messageToMediationService,token);
-		
-		
+		diff = after - before;
+		logger.info("Time parsing = " + diff + " millisec");
+		messageToMediationService.setParseApproved(isApproved);
+		addCommentToMediationService(messageToMediationService, token);
+
 		return isApproved;
-		
-		
+
 	}
-	
-	
-	
-	
-	private void addCommentToMediationService(
-			MessageToMediationService messageToMediationService, String token)  {
-		
-		
+
+	public boolean remoteValidateComment(String testoentity, int identity,
+			Long userid, String token) {
+
+		MessageToMediationService messageToMediationService = new MessageToMediationService(
+				webappname, identity, testoentity, String.valueOf(userid));
+
+		boolean isApproved = true;
+
+		messageToMediationService.setParseApproved(isApproved);
+		addCommentToMediationService(messageToMediationService, token);
+
+		return isApproved;
+
+	}
+
+	private List<KeyWordPersistent> updateKeyWord(String token, long data) {
+
 		try {
-			logger.debug(urlServermediation+MediationConstant.ADD_COMMENT);
-			RemoteConnector.postJSON(urlServermediation, MediationConstant.ADD_COMMENT, messageToMediationService.ToJson(), token);
+			logger.debug(urlServermediation + MediationConstant.ADD_COMMENT);
+			String response = RemoteConnector.getJSON(urlServermediation,
+					MediationConstant.GET_KEYWORD + data, token);
+			
+			List<KeyWordPersistent> keyWordPersistentList=KeyWordPersistent.valueOfList(response);
+			
+			return keyWordPersistentList;
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	private void addCommentToMediationService(
+			MessageToMediationService messageToMediationService, String token) {
+
+		try {
+			logger.debug(urlServermediation + MediationConstant.ADD_COMMENT);
+			RemoteConnector.postJSON(urlServermediation,
+					MediationConstant.ADD_COMMENT,
+					messageToMediationService.ToJson(), token);
 		} catch (SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -107,16 +129,15 @@ public class MediationParserImpl {
 			e.printStackTrace();
 		}
 
-		
 	}
 
 	// restituisce la lista delle parole da filtrare
 	private Collection<String> getNotApprovedWordDictionary() {
-		Collection<String> stringColl=new ArrayList<String>();
-		
+		Collection<String> stringColl = new ArrayList<String>();
+
 		TextReader readerBW = new TextReader();
 		stringColl = readerBW.getListFromFiles();
-		
+
 		return stringColl;
 	}
 
--- src/main/java/eu/trentorise/smartcampus/mediation/util/MediationConstant.java
+++ src/main/java/eu/trentorise/smartcampus/mediation/util/MediationConstant.java
@@ -3,5 +3,6 @@ package eu.trentorise.smartcampus.mediation.util;
 public class MediationConstant {
 
 	public static final String ADD_COMMENT = "/rest/comment/parseapproved/add";
+	public static final String GET_KEYWORD = "/rest/key/";
 
 }
